935d6af2f9dd71ce9e14754e3ccc81ab4b86806c
Francisco Ferro <fcoferro@gmail.com>
2026-07-17 15:19:20 -0700

    Create POST

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewAPI.Models;
+
+namespace NewAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonController : Controller
+    {
+        [HttpPost]
+        public IActionResult CreatePerson([FromBody] PersonRequest request)
+        {
+            if (request == null)
+                return BadRequest("Invalid request");
+
+            var response = new
+            {
+                Message = "Persona registrada correctamente",
+                Data = request
+            };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ Models/PersonRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace NewAPI.Models
+{
+    public class PersonRequest
+    {
+        public string Name { get; set; }
+        public string Perfil { get; set; }
+        public string SSN { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
